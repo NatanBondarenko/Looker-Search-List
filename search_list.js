410c55ea1dea670c698d82f6b70e04aa2a02412d
NatanBondarenko <97616282+NatanBondarenko@users.noreply.github.com>
2023-06-04 15:15:56 +0300

    Update search_list.js

--- search_list.js
+++ search_list.js
@@ -1,96 +1,89 @@
-import * as d3 from './d3loader';
-import { VisPluginTableModel } from './vis_table_plugin';
+looker.plugins.visualizations.add({
+  options: {},
+  create: function(element, config) {
+    // Create the search bar
+    const searchBar = document.createElement('div');
+    searchBar.classList.add('search-bar');
 
-const createTableFromData = (data) => {
-  const visContainer = d3.select('#visContainer').node();
-  if (!visContainer) {
-    d3.select('#vis')
-      .append('div')
-      .attr('id', 'visContainer')
-      .style('height', '80vh')
-      .style('display', 'flex')
-      .style('align-items', 'center')
-      .style('justify-content', 'center')
-      .style('font-size', '13px')
-      .style('font-family', '"Open Sans", "Helvetica", "Arial", sans-serif')
-      .style('text-anchor', 'middle')
-      .style('text-align', 'center')
-      .text('No Results');
-  } else {
-    d3.select('#visContainer').html('').text('No Results');
+    const searchInput = document.createElement('input');
+    searchInput.setAttribute('type', 'text');
+    searchInput.setAttribute('id', 'searchInput');
+    searchInput.setAttribute('placeholder', 'Search help topics...');
+    searchInput.addEventListener('input', searchHelp);
+
+    searchBar.appendChild(searchInput);
+
+    // Create the help sections
+    const helpSections = [];
+
+    // Function to toggle section visibility
+    function toggleSection(sectionId) {
+      const content = document.getElementById(sectionId);
+      if (content.style.display === 'none') {
+        content.style.display = 'block';
+      } else {
+        content.style.display = 'none';
+      }
+    }
+
+    // Function to perform search on help topics
+    function searchHelp() {
+      const input = document.getElementById('searchInput').value.toLowerCase();
+      const sections = document.getElementsByClassName('help-section');
+
+      for (let i = 0; i < sections.length; i++) {
+        const content = sections[i].getElementsByClassName('help-section-content')[0];
+        const matches = content.innerText.toLowerCase().includes(input);
+
+        if (matches) {
+          sections[i].style.display = 'block';
+        } else {
+          sections[i].style.display = 'none';
+        }
+      }
+    }
+
+    // Generate the help section HTML dynamically
+    const helpSectionData = [
+      {
+        header: 'Offers Analysis',
+        description: 'Offers Analysis includes all offers generated for application - Don\'t support latest offer!',
+      },
+      {
+        header: 'Flow Analysis (Latest Offer)',
+        description: 'Production data: from applications to loans',
+      },
+      // Add more help sections as needed
+    ];
+
+    helpSectionData.forEach((sectionData, index) => {
+      const sectionId = `section${index + 1}`;
+
+      const sectionContainer = document.createElement('div');
+      sectionContainer.classList.add('help-section');
+
+      const sectionHeader = document.createElement('div');
+      sectionHeader.classList.add('help-section-header');
+      sectionHeader.textContent = sectionData.header;
+      sectionHeader.addEventListener('click', () => toggleSection(sectionId));
+
+      const sectionContent = document.createElement('div');
+      sectionContent.classList.add('help-section-content');
+      sectionContent.id = sectionId;
+      sectionContent.textContent = sectionData.description;
+
+      sectionContainer.appendChild(sectionHeader);
+      sectionContainer.appendChild(sectionContent);
+
+      helpSections.push(sectionContainer);
+    });
+
+    // Append the elements to the visualization container
+    element.appendChild(searchBar);
+    helpSections.forEach(section => element.appendChild(section));
+  },
+  updateAsync: function(data, element, config, queryResponse, details, doneRendering) {
+    // No data manipulation or updates needed for this visualization
+    doneRendering();
   }
-
-  const dataTable = new VisPluginTableModel(data);
-  const config = {
-    theme: 'looker',
-    layout: 'auto',
-    customTheme: null,
-    headerFontSize: 12,
-    bodyFontSize: 12,
-    showHighlight: true,
-    showTooltip: true,
-  };
-
-  const removeStyles = async function () {
-    const links = document.getElementsByTagName('link');
-    while (links[0]) links[0].parentNode.removeChild(links[0]);
-
-    Object.keys(themes).forEach(async (theme) => await themes[theme].unuse());
-  };
-
-  const loadStylesheet = function (link) {
-    const linkElement = document.createElement('link');
-    linkElement.setAttribute('rel', 'stylesheet');
-    linkElement.setAttribute('href', link);
-    document.getElementsByTagName('head')[0].appendChild(linkElement);
-  };
-
-  const themes = {
-    traditional: require('./theme_traditional.css'),
-    looker: require('./theme_looker.css'),
-    contemporary: require('./theme_contemporary.css'),
-    fixed: require('./layout_fixed.css'),
-    auto: require('./layout_auto.css'),
-  };
-
-  const buildReportTable = function () {
-    const table = d3
-      .select('#visContainer')
-      .append('table')
-      .attr('id', 'reportTable')
-      .attr('class', 'reportTable')
-      .style('opacity', 0);
-
-    // ... Rest of the code from the original table code ...
-
-    // Replace the remaining code with your own logic to populate the table using "data"
-
-    return table;
-  };
-
-  removeStyles().then(() => {
-    if (
-      typeof config.customTheme !== 'undefined' &&
-      config.customTheme &&
-      config.theme === 'custom'
-    ) {
-      loadStylesheet(config.customTheme);
-    } else if (typeof themes[config.theme] !== 'undefined') {
-      themes[config.theme].use();
-    }
-    if (typeof themes[config.layout] !== 'undefined') {
-      themes[config.layout].use();
-    }
-  });
-
-  buildReportTable();
-};
-
-// Example usage
-const data = [
-  { name: 'John', age: 30 },
-  { name: 'Jane', age: 25 },
-  { name: 'Bob', age: 40 },
-];
-
-createTableFromData(data);
+});
