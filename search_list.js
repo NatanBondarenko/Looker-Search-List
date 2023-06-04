343e738c5670831617a0811baed5a9ca1c66435b
NatanBondarenko <97616282+NatanBondarenko@users.noreply.github.com>
2023-06-04 13:17:37 +0300

    Update search_list.js

--- search_list.js
+++ search_list.js
@@ -1,7 +1,7 @@
 looker.plugins.visualizations.add({
   options: {},
   create: function(element, config) {
-    // Define the help sections as a nested object
+    // Define the help sections as an array of objects
     const helpSections = [
       {
         title: 'Offers Analysis',
@@ -14,27 +14,34 @@ looker.plugins.visualizations.add({
       // Add more help sections as needed
     ];
 
-    // Create the tree structure for help sections
-    const tree = document.createElement('ul');
-    tree.classList.add('help-tree');
+    // Create the list structure for help sections
+    const list = document.createElement('ul');
+    list.classList.add('help-list');
 
     helpSections.forEach((section, index) => {
       const listItem = document.createElement('li');
-      listItem.innerHTML = `
-        <span class="help-section-title">${section.title}</span>
-        <div class="help-section-content">${section.content}</div>
-      `;
       listItem.classList.add('help-section');
 
-      listItem.addEventListener('click', () => {
-        listItem.classList.toggle('active');
+      const header = document.createElement('div');
+      header.classList.add('help-section-header');
+      header.textContent = section.title;
+
+      const content = document.createElement('div');
+      content.classList.add('help-section-content');
+      content.textContent = section.content;
+
+      listItem.appendChild(header);
+      listItem.appendChild(content);
+
+      listItem.addEventListener('click', function() {
+        this.classList.toggle('active');
       });
 
-      tree.appendChild(listItem);
+      list.appendChild(listItem);
     });
 
-    // Append the tree to the element
-    element.appendChild(tree);
+    // Append the list to the element
+    element.appendChild(list);
   },
   updateAsync: function(data, element, config, queryResponse, details, doneRendering) {
     // No data manipulation or updates needed for this visualization
