85133aa31a8b55372bb473b11459e350aa6119f7
NatanBondarenko <97616282+NatanBondarenko@users.noreply.github.com>
2023-06-04 14:23:24 +0300

    Update search_list.js

--- search_list.js
+++ search_list.js
@@ -1,29 +1,22 @@
 looker.plugins.visualizations.add({
-  options: {
-    sections: {
-      type: 'array',
-      label: 'Sections',
-      section: 'Content',
-      display: 'table',
-      default: [],
-      order: 1,
-      rows: {
-        header: {
-          type: 'string',
-          label: 'Header',
-        },
-        description: {
-          type: 'string',
-          label: 'Description',
-        },
-      },
-    },
-  },
+  options: {},
   create: function(element, config) {
-    // Create the help section elements
+    // Create the search bar
+    const searchBar = document.createElement('div');
+    searchBar.classList.add('search-bar');
+
+    const searchInput = document.createElement('input');
+    searchInput.setAttribute('type', 'text');
+    searchInput.setAttribute('id', 'searchInput');
+    searchInput.setAttribute('placeholder', 'Search help topics...');
+    searchInput.addEventListener('input', searchHelp);
+
+    searchBar.appendChild(searchInput);
+
+    // Create the help sections
     const helpSections = [];
 
-    // Add click event listener to toggle visibility
+    // Function to toggle section visibility
     function toggleSection(sectionId) {
       const content = document.getElementById(sectionId);
       if (content.style.display === 'none') {
@@ -33,8 +26,37 @@ looker.plugins.visualizations.add({
       }
     }
 
-    // Generate the help section HTML dynamically based on options
-    config.sections.forEach((section, index) => {
+    // Function to perform search on help topics
+    function searchHelp() {
+      const input = document.getElementById('searchInput').value.toLowerCase();
+      const sections = document.getElementsByClassName('help-section');
+
+      for (let i = 0; i < sections.length; i++) {
+        const content = sections[i].getElementsByClassName('help-section-content')[0];
+        const matches = content.innerText.toLowerCase().includes(input);
+
+        if (matches) {
+          sections[i].style.display = 'block';
+        } else {
+          sections[i].style.display = 'none';
+        }
+      }
+    }
+
+    // Generate the help section HTML dynamically
+    const helpSectionData = [
+      {
+        header: 'Offers Analysis',
+        description: 'Offers Analysis includes all offers generated for application - Don\'t support latest offer!',
+      },
+      {
+        header: 'Flow Analysis (Latest Offer)',
+        description: 'Production data: from applications to loans',
+      },
+      // Add more help sections as needed
+    ];
+
+    helpSectionData.forEach((sectionData, index) => {
       const sectionId = `section${index + 1}`;
 
       const sectionContainer = document.createElement('div');
@@ -42,13 +64,13 @@ looker.plugins.visualizations.add({
 
       const sectionHeader = document.createElement('div');
       sectionHeader.classList.add('help-section-header');
-      sectionHeader.textContent = section.header;
+      sectionHeader.textContent = sectionData.header;
       sectionHeader.addEventListener('click', () => toggleSection(sectionId));
 
       const sectionContent = document.createElement('div');
       sectionContent.classList.add('help-section-content');
       sectionContent.id = sectionId;
-      sectionContent.textContent = section.description;
+      sectionContent.textContent = sectionData.description;
 
       sectionContainer.appendChild(sectionHeader);
       sectionContainer.appendChild(sectionContent);
@@ -56,7 +78,8 @@ looker.plugins.visualizations.add({
       helpSections.push(sectionContainer);
     });
 
-    // Append the help sections to the element
+    // Append the elements to the visualization container
+    element.appendChild(searchBar);
     helpSections.forEach(section => element.appendChild(section));
   },
   updateAsync: function(data, element, config, queryResponse, details, doneRendering) {
